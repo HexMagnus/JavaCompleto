abf85a2a03542b8ab104d157cd5eec0a6f8d04fc
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-11-01 22:36:30 -0300

    Update Main.java

--- EstruturasCondicionais/src/estruturaCondicional/Main.java
+++ EstruturasCondicionais/src/estruturaCondicional/Main.java
@@ -1,12 +1,56 @@
 package estruturaCondicional;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		int hora;
+				
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Quantas horas?");
+		hora = sc.nextInt();
+		
+		if (hora < 12 ) {
 		System.out.println("Bom dia");
-		System.out.println("Bom tarde");
-		System.out.println("Bom noite");
+		}
+		else {
+			if(hora  <18) {
+			System.out.println("Boa tarde");
+			}
+			else {
+			}
+		System.out.println("Boa tarde");
+		
+		sc.close();
+		
+		}
 	}
 
 }
+
+
+//V: executa somente o bloco do if
+//F: executa somente o bloco do else
+
+//if (condicao1) {
+//	comando 1
+//	comando 2
+//}
+
+//else {
+
+// if(condicao 2){
+//	comando 3
+//	comando 4
+//}
+
+//else{
+// comando 5
+// comando 6
+
+//}
+
+//}
