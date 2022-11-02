606c76f74c80341ad1477e9d5ed0832d44baf397
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-11-01 22:13:30 -0300

    1 comit

---
+++ EstruturasCondicionais/src/estruturaCondicional/Main.java
@@ -0,0 +1,12 @@
+package estruturaCondicional;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bom dia");
+		System.out.println("Bom tarde");
+		System.out.println("Bom noite");
+	}
+
+}
